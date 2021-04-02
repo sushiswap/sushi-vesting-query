eaaebfeac38b72356c9e9e0da819c7122d28e37d
LufyCZ <lukasczk@seznam.cz>
2021-04-02 15:40:23 +0200

    Clean up + update blacklist

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,13 @@ export const VESTING_START = 10959148;
 
 export const VESTING_POOL_ID = 29;
 
-export const apiKey = ''
-export const alchemyKey = ''
+export const MULTISIGS = [
+    "0xaa394e08c74e2b2f76ff000adeb25f20423cfe2a",
+    "0x4a25e18076ddcfd646ed14abc07286c2a4c1256a",
+    "0x34ddfc06ce0c39242fb380066ee01e409a4a525e",
+    "0x4a49985b14bd0ce42c25efde5d8c379a48ab02f3",
+    "0x859eefc267671595d987d0f6589d7771d4877113"
+];
+
+export const etherscanKey = 'G1MBF34MJ6K59Y4X8VUQ7CQC4NB6BV2R35'
+export const alchemyKey = 'htNN-ROB3O_PECCan9Ll-eXTc0JlXz_b'
